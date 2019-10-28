9e6436c35471581e685c5c3b92b91b9eaf039edb
thiago.santos <silva.thiago@totvs.com.br>
2019-10-28 14:50:59 -0300

    Criando componentes

--- bookmarks/src/hello.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Hello(props) {
-    return (<h1>Olá</h1>)
-}
-
-export default Hello;
--- bookmarks/src/stories/index.stories.js
+++ bookmarks/src/stories/index.stories.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Hello from '../hello';
+import Hello from '../components/hello';
+import SimpleList from '../components/SimpleList'
 
 storiesOf('Testes', module)
     .add('Hello', ()=> <Hello/>)
+    .add('SimpleList', () => <SimpleList value="d"/>)
