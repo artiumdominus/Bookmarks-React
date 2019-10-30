931b048d6ee9012f355db0a1f7c28ac9bcbd0f0c
thiago.santos <silva.thiago@totvs.com.br>
2019-10-30 16:53:11 -0300

    Delete old project

--- bookmarks/src/stories/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Hello from '../components/hello';
-import SimpleList from '../components/SimpleList'
-
-storiesOf('Testes', module)
-    .add('Hello', ()=> <Hello/>)
-    .add('SimpleList', () => <SimpleList value="d"/>)
