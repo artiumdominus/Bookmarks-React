d1047ece65a9ac218f63fc33a9cdd2d5eaa6e5e7
thiago.santos <silva.thiago@totvs.com.br>
2019-10-30 16:51:20 -0300

    Login criado

---
+++ booksmark/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './components/login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login>
+
+      </Login>
+    </div>
+  );
+}
+
+export default App;
