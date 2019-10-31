963496cfcac3df290397f20e5d9dc209811cf1a6
thiagosantos346 <thiagosantos346@gmail.com>
2019-10-31 00:17:35 -0300

    ed login

--- booksmark/src/components/login/login.js
+++ booksmark/src/components/login/login.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { Form, Button, FormField, Input, Container, Segment, Advertisement, Grid} from 'semantic-ui-react'
-
+import './login.css'
 
 export default class Login extends React.Component{
 
     render(){
 
         return (
-            < Grid verticalAlign='top' celled='internally' centered >
-                <Grid.Row centered columns={2}>
-                    <Grid.Column>
-                        <Form>
-                            <Segment.Group stacked >
-                                    <Segment>
-                                    <FormField>
-                                        <Input icon='user' iconPosition='left' placeholder='example@example.com' />
-                                    </FormField>
-                                    </Segment>
-                                    <Segment>
-                                    <FormField>
-                                        <Input icon='unlock' iconPosition='left' placeholder='Password' />
-                                    </FormField>
-                                    </Segment>
-                                    <Segment> 
-                                    <Button fluid positive type='submit'>Login</Button>
-                                    </Segment>
-                            </Segment.Group>
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+            <div className='login'>
+                < Grid verticalAlign='top' celled='internally' centered >
+                    <Grid.Row centered columns={3}>
+                        <Grid.Column>
+                            <Form>
+                                <Segment.Group stacked >
+                                        <Segment>
+                                        <FormField>
+                                            <Input icon='user' iconPosition='left' placeholder='example@example.com' />
+                                        </FormField>
+                                        </Segment>
+                                        <Segment>
+                                        <FormField>
+                                            <Input type="password" icon='unlock' iconPosition='left' placeholder='Password' />
+                                        </FormField>
+                                        </Segment>
+                                        <Segment> 
+                                        <Button fluid positive type='submit'>Login</Button>
+                                        </Segment>
+                                </Segment.Group>
+                            </Form>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
         )
 
     }
